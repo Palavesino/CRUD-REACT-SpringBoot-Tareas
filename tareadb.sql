68800004a666336cf4c7a1674f362d0889b99225
Facundo Palavesino <palavesinofacundo@gmail.com>
2023-05-03 18:56:46 -0300

    agregando sql de bd

---
+++ tareadb.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE tareadb;
+/* Insertar datos va ver si esta funcionando el front
+INSERT INTO tarea (nombre, completado) VALUES ('Comprar leche', false),('Llamar al dentista', true),('Hacer ejercicio', true);
+SELECT * FROM tareadb.tarea;
+*/
